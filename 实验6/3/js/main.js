123b726dea03e2ec2af76f1352accaba1156ab47
dreamhuan <fkq_love_chy@163.com>
2017-04-14 19:24:27 +0800

    实验6  一个小bug

--- 实验6/3/js/main.js
+++ 实验6/3/js/main.js
@@ -13,10 +13,11 @@ $(function () {
         // console.log($($newItem.children()[0]).text());
         $($newItem.children()[0]).text(count + 1);
         $("#items").append($newItem);
-        $(".delete").click(del);
+        $(".delete").unbind('click').click(del); //事件绑定不是替代，是叠加，所以先解绑
     });
 
-    function del() {
+    function del(e) {
+        //console.log(1); //16行直接绑定的话前面的几个item会绑定好多删除事件，会输出好多1
         $(this).parent().remove();
         let len = $(".ui-box-item-index").length;
         for (let i = 0; i < len; i++) {
